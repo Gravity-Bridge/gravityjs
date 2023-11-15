e36a7a63d8ce3e3410991c587aa21469632cefa4
Christian Borst <christianoborst@gmail.com>
2023-11-15 10:37:49 -0500

    Fix proto pkg gravity exports and tests

--- packages/proto/src/messages/gravity/gravity.spec.ts
+++ packages/proto/src/messages/gravity/gravity.spec.ts
@@ -1,20 +1,55 @@
-import { createMsgBid } from './msgSendToEth'
-import { MsgBid } from '../../proto/auction/v1/msgs_pb'
+import { createMsgSendToEth } from './msgSendToEth'
+import { createMsgCancelSendToEth } from './msgCancelSendToEth'
+import { createMsgRequestBatch } from './msgRequestBatch'
+import { createMsgExecuteIbcAutoForwards } from './msgExecuteIbcAutoForwards'
+
+import { MsgSendToEth, MsgCancelSendToEth, MsgRequestBatch, MsgExecuteIbcAutoForwards } from '../../proto/gravity/v1/msgs_pb'
 import { from, to, denom } from '../../proto/tests/utils'
 import { JSONOptions } from '../../registry/registry'
 
-describe('test Auction Module message generation', () => {
-  it('correctly wraps MsgBid', () => {
-    const id = '1'
+describe('test Gravity Module message generation', () => {
+  it('correctly wraps MsgSendToEth', () => {
     const amount = '10000000'
-    const msg = createMsgBid(id, from, amount, amount)
+    const msg = createMsgSendToEth(from, to, amount, denom, amount, amount)
 
     expect(msg.message.toJson(JSONOptions)).toStrictEqual({
-      auction_id: id,
-      bidder: from,
-      amount: amount,
-      bid_fee: amount,
+      sender: from,
+      eth_dest: to,
+      amount,
+      denom,
+      bridge_fee: amount,
+      chain_fee: amount,
     })
-    expect(msg.path).toStrictEqual(MsgBid.typeName)
+    expect(msg.path).toStrictEqual(MsgSendToEth.typeName)
+  })
+
+  it('correctly wraps MsgCancelSendToEth', () => {
+    const msg = createMsgCancelSendToEth(from)
+
+    expect(msg.message.toJson(JSONOptions)).toStrictEqual({
+      sender: from,
+    })
+    expect(msg.path).toStrictEqual(MsgCancelSendToEth.typeName)
+  })
+
+  it('correctly wraps MsgRequestBatch', () => {
+    const msg = createMsgRequestBatch(from, denom)
+
+    expect(msg.message.toJson(JSONOptions)).toStrictEqual({
+      sender: from,
+      denom,
+    })
+    expect(msg.path).toStrictEqual(MsgRequestBatch.typeName)
+  })
+
+  it('correctly wraps MsgExecuteIbcAutoForwards', () => {
+    const amount = '10000000'
+    const msg = createMsgExecuteIbcAutoForwards(from, amount)
+
+    expect(msg.message.toJson(JSONOptions)).toStrictEqual({
+      sender: from,
+      forwards_to_clear: amount,
+    })
+    expect(msg.path).toStrictEqual(MsgExecuteIbcAutoForwards.typeName)
   })
 })
--- packages/proto/src/messages/gravity/index.ts
+++ packages/proto/src/messages/gravity/index.ts
@@ -1,3 +1,7 @@
 export * from './msgSendToEth'
+export * from './msgCancelSendToEth'
+export * from './msgRequestBatch'
+export * from './msgExecuteIbcAutoForwards'
+
 export * from './registry'
 export * from './aminoRegistry'
